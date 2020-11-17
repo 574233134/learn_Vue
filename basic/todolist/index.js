6d9bc739ab0b420b1b241b49137cd3a88959f0b3
李梦珂 <limengke1@jd.com>
2020-11-17 15:55:45 +0800

    vue demo

---
+++ basic/todolist/index.js
@@ -0,0 +1,11 @@
+import vue from '../vue'
+import App from './App'
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+    el: '#app',
+    template: '<App/>',
+    components: { App }
+})
+
